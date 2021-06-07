5910d6ada0b09c8ab2d251bdaa4a07c02a20406a
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-07 19:13:09 -0400

    Update Pokecard.js
    import api

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import './Pokecard.css';
 
+
+const POKE_API =
+  "https://raw.githubusercontent.com/" +
+    "PokeAPI/sprites/master/sprites/pokemon/";
+  
+    console.log(POKE_API)
 
