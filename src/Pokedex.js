50ffcc081e6da3442362d799299d82e3e9981148
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-07 20:50:24 -0400

    update

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,29 +4,29 @@ import "./Pokedex.css";
 
 /** Index of Pokemon: show individual cards. */
 
-functon Pokedex(props) {
-    let winMessage = null;
-    if (props.isWinner) {
-        winMessage = <p className="Pokedex-winner">THIS HAND WINS</p>;
-    }
-    return (
-        <div className="Pokedex">
-            <h2 className="Pokedex-title">Pokedex</h2>
-            <div className="Pokedex-cards">
-                {props.pokemon.map(p => (
-                    <Pokecard
-                        id={p.id}
-                        name={p.name}
-                        type={p.type}
-                        exp={p.base_experience}
-                    />
-                ))}
-            </div>
-            <h4>Total experience: {props.exp}</h4>
-            {winMessage}
-        </div>
-        
-    );
+function Pokedex(props) {
+  let winMessage = null;
+  if (props.isWinner) {
+    winMessage = <p className="Pokedex-winner">THIS HAND WINS!</p>;
+  }
+
+  return (
+    <div className="Pokedex">
+      <h2 className="Pokedex-title">Pokedex</h2>
+      <div className="Pokedex-cards">
+        {props.pokemon.map((p) => (
+          <Pokecard
+            id={p.id}
+            name={p.name}
+            type={p.type}
+            exp={p.base_experience}
+          />
+        ))}
+      </div>
+      <h4>Total experience: {props.exp}</h4>
+      {winMessage}
+    </div>
+  );
 }
 
 export default Pokedex;
