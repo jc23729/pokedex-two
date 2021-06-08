a7cde2a75cfda3d6d4e691ad544cbbcf2f7a2700
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-07 20:49:40 -0400

    update
    comparing to solution

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -11,7 +11,19 @@ functon Pokedex(props) {
     }
     return (
         <div className="Pokedex">
-            <h2 className = "Pokedex-title">Pokedex</h2>
+            <h2 className="Pokedex-title">Pokedex</h2>
+            <div className="Pokedex-cards">
+                {props.pokemon.map(p => (
+                    <Pokecard
+                        id={p.id}
+                        name={p.name}
+                        type={p.type}
+                        exp={p.base_experience}
+                    />
+                ))}
+            </div>
+            <h4>Total experience: {props.exp}</h4>
+            {winMessage}
         </div>
         
     );
