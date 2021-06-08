ab321b92196f2e429789bcfb149cc28a600569f9
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-07 20:30:10 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import logo from './logo.svg';
 import Pokecard from "./Pokecard.js";
 
-function App(props) {
+function App() {
   return (
     <div className="App">
-      <div className="Pokedex-cards">
-        {props.pokemon.map((p) => (
-          <Pokecard
-            id={p.id}
-            name={p.name}
-            type={p.type}
-            exp={p.base_experience}
-          />
-        ))}
-      </div>
+    
     </div>
   );
 }
