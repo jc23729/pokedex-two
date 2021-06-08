7d5627c7e95a38e76e114104e33a2a4747590bd5
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-07 21:05:13 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
-import Pokecard from "./Pokecard.js";
+import Pokedex from "./Pokedex";
 
 function App() {
   return (
     <div className="App">
-    
+  
     </div>
   );
 }
