8421c43e6ee9e4448c9e444a52da7b01c508b442
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-07 20:37:11 -0400

    Update Pokedex.js

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,3 +1,20 @@
 import React from "react";
 import Pokecard from "./Pokecard";
 import "./Pokedex.css";
+
+/** Index of Pokemon: show individual cards. */
+
+functon Pokedex(props) {
+    let winMessage = null;
+    if (props.isWinner) {
+        winMessage = <p className="Pokedex-winner">THIS HAND WINS</p>;
+    }
+    return (
+        <div className="Pokedex">
+            <h2 className = "Pokedex-title">Pokedex</h2>
+        </div>
+        
+    );
+}
+
+export default Pokedex;
